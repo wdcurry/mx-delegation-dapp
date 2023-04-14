dcdedf36bdf19d7cd79d17a97669939ec52d1b2e
drew curry <drew.curry@me.com>
2023-04-14 17:57:29 -0600

    Update config.mainnet.ts
    convert out ApeStaking contract.

--- src/config.mainnet.ts
+++ src/config.mainnet.ts
@@ -52,7 +52,7 @@ export const network: NetworkType = {
   gatewayAddress: 'https://gateway.multiversx.com',
   explorerAddress: 'https://explorer.multiversx.com',
   delegationContract:
-    'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqx8llllsxavffq'
+    'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqn8lllls0e2ea0'
 };
 
 export const delegationContractData: DelegationContractType[] = [
